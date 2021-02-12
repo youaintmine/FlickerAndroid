0d2a750bc77d10935bbcbe5e69bec67178ea1cf3
ahmbarish <ahmbarishsaikia@gmail.com>
2021-02-12 11:21:03 +0530

    Parcelable

--- app/src/main/java/com/ahmbarish/flickerbrowser/Photo.kt
+++ app/src/main/java/com/ahmbarish/flickerbrowser/Photo.kt
@@ -1,43 +1,77 @@
 package com.ahmbarish.flickerbrowser
 
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.Log
 import java.io.*
 import java.io.DataInputStream.readUTF
 
-class Photo(var title: String, var author: String,var authorId: String,var link: String,var tags: String,var image: String) : Serializable {
+class Photo(var title: String, var author: String,var authorId: String,var link: String,var tags: String,var image: String) : Parcelable {
 
-    companion object {
-        private const val serialVersionUID = 1L
+//    companion object {
+//        private const val serialVersionUID = 1L
+//    }
+
+    constructor(parcel: Parcel) : this(
+            parcel.readString().toString(),
+            parcel.readString().toString(),
+            parcel.readString().toString(),
+            parcel.readString().toString(),
+            parcel.readString().toString(),
+            parcel.readString().toString()) {
     }
 
     override fun toString(): String {
         return "Photo(title='$title', author='$author', authorId='$authorId', link='$link', tags='$tags', image='$image')"
     }
 
-    @Throws(IOException::class)
-    private fun writeObject(out : java.io.ObjectOutputStream) {
-        Log.d("Photo","writeObject called")
-        out.writeUTF(title)
-        out.writeUTF(author)
-        out.writeUTF(authorId)
-        out.writeUTF(link)
-        out.writeUTF(tags)
-        out.writeUTF(image)
+
+//    @Throws(IOException::class)
+//    private fun writeObject(out : java.io.ObjectOutputStream) {
+//        Log.d("Photo","writeObject called")
+//        out.writeUTF(title)
+//        out.writeUTF(author)
+//        out.writeUTF(authorId)
+//        out.writeUTF(link)
+//        out.writeUTF(tags)
+//        out.writeUTF(image)
+//    }
+//
+//    @Throws(IOException::class, ClassNotFoundException::class)
+//    private fun readObject(inputStream : java.io.ObjectInputStream) {
+//        Log.d("Photo", "readObject called")
+//        title = inputStream.readUTF()
+//        author = inputStream.readUTF()
+//        authorId = inputStream.readUTF()
+//        link = inputStream.readUTF()
+//        tags = inputStream.readUTF()
+//        image = inputStream.readUTF()
+//    }
+//
+//    @Throws(ObjectStreamException::class)
+//    private fun readObjectNoData() {
+//        Log.d("Photo","readObjectNoData called")
+//    }
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(author)
+        parcel.writeString(authorId)
+        parcel.writeString(link)
+        parcel.writeString(tags)
+        parcel.writeString(image)
     }
 
-    @Throws(IOException::class, ClassNotFoundException::class)
-    private fun readObject(inputStream : java.io.ObjectInputStream) {
-        Log.d("Photo", "readObject called")
-        title = inputStream.readUTF()
-        author = inputStream.readUTF()
-        authorId = inputStream.readUTF()
-        link = inputStream.readUTF()
-        tags = inputStream.readUTF()
-        image = inputStream.readUTF()
+    override fun describeContents(): Int {
+        return 0
     }
 
-    @Throws(ObjectStreamException::class)
-    private fun readObjectNoData() {
-        Log.d("Photo","readObjectNoData called")
+    companion object CREATOR : Parcelable.Creator<Photo> {
+        override fun createFromParcel(parcel: Parcel): Photo {
+            return Photo(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Photo?> {
+            return arrayOfNulls(size)
+        }
     }
 }
--- app/src/main/java/com/ahmbarish/flickerbrowser/PhotoDetailsActivity.kt
+++ app/src/main/java/com/ahmbarish/flickerbrowser/PhotoDetailsActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.content_photo_details.*
 
 class PhotoDetailsActivity : BaseClass() {
 
@@ -11,6 +13,18 @@ class PhotoDetailsActivity : BaseClass() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.photo_details)
         activateToolbar(true)
+
+//        val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
+            val photo = intent.extras?.getParcelable<Photo>(PHOTO_TRANSFER) as Photo
+        photo_title.text = photo.title
+        photo_tags.text = photo.tags
+        photo_author.text = photo.author
+
+        //Picasso from BindView
+        Picasso.get().load(photo.link)
+                .error(R.drawable.placeholder_48)
+                .placeholder(R.drawable.placeholder_48)
+                .into(photo_image)
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
